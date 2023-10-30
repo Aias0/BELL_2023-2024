884b4fc3d72d4ac6c7d42e4ba4f765f4c6f5ccc4
Quentin Balestri <quentinbalestri@gmail.com>
2023-10-30 15:23:27 -0500

    thermal tracking test works

--- AVR/Test_Code/thermal_tracker.py
+++ AVR/Test_Code/thermal_tracker.py
@@ -1,28 +1,38 @@
-import cv2, time
+import cv2, time, json, base64, scipy, statistics
 import numpy as np
 from scipy import ndimage
+from bell.avr.mqtt.payloads import AvrThermalReadingPayload
+
+target_range = (23, 40)
+
 test_imgs = ['Misc/Thermal_Images/unnamed.png', "Misc/Thermal_Images/fnp-toc-default-thermal-imaging.jpg", "Misc/Thermal_Images/thermal-hot-spot-300x224.jpg", "Misc/Thermal_Images/HotSpot-thermal-image.jpg", "Misc/Thermal_Images/eyJidWNrZXQiOiJ3ZXZvbHZlci1wcm9qZWN0LWltYWdlcyIsImtleSI6ImZyb2FsYS8xNjI5ODg4Nzk4MzYzLWNpcmN1aXQtcGFuZWwtMi5qcGciLCJlZGl0cyI6eyJyZXNpemUiOnsid2lkdGgiOjk1MCwiZml0IjoiY292ZXIifX19.webp"]
 
-thermal_image = cv2.imread(test_imgs[0])
-thermal_image = cv2.resize(thermal_image, (720, 480))
-cv2.imshow('Input', cv2.resize(thermal_image, (720, 480)))
-#thermal_image = np.asarray(self.thermal_pixel_matrix, dtype=np.uint8)
-lowerb = np.array([0, 0, 230], np.uint8)
-upperb = np.array([255, 255, 255], np.uint8)
-frame = cv2.inRange(thermal_image, lowerb, upperb)
-blobs = frame > 100
+payload = AvrThermalReadingPayload(data= 'FBUWFhYVFhUUFhYZGBYVFRQVFhcaGBUUFRUWGCAaFxYUFRYXGhgWFRQVFRYXFhUUExQUFhYWFRQTExQVFRQUEw==')
+payload = {"data": "FBYWFxYWFhYUFhcaGBcWFhUWFxgaGBYWFRYWGSEaFxYVFhYXHBgWFxUWFhYYFxYWFBUWFhYWFRQUFBUVFhQVFA=="}
+
+#data = json.loads(payload)["data"]
+data = payload['data']
+# decode the payload
+base64Decoded = data.encode("utf-8")
+asbytes = base64.b64decode(base64Decoded)
+pixel_ints = list(bytearray(asbytes))
+thermal_grid = [[0 for _ in range(8)] for _ in range(8)]
+k = 0
+for i in range(len(thermal_grid)):
+    for j in range(len(thermal_grid[0])):
+        thermal_grid[i][j] = pixel_ints[k]
+        k+=1
+img = np.array(thermal_grid)
+lowerb = np.array(target_range[0], np.uint8)
+upperb = np.array(target_range[1], np.uint8)
+mask = cv2.inRange(img, lowerb, upperb)
+print(mask)
+blobs = mask > 100
 labels, nlabels = ndimage.label(blobs)
 # find the center of mass of each label
-t = ndimage.center_of_mass(frame, labels, np.arange(nlabels) + 1 )
+t = ndimage.center_of_mass(mask, labels, np.arange(nlabels) + 1 )
 # calc sum of each label, this gives the number of pixels belonging to the blob
 s  = ndimage.sum(blobs, labels,  np.arange(nlabels) + 1 )
 # print the center of mass of the largest blob
 center_of_mass = [int(x) for x in t[s.argmax()][::-1]]
 print(center_of_mass) # notation of output (y,x)
-targeting_img =  cv2.cvtColor(frame,cv2.COLOR_GRAY2RGB)
-targeting_img = cv2.circle(targeting_img, center_of_mass, radius=4, color=(0, 0, 255), thickness=-1)
-cv2.imshow('Mask', targeting_img)
-cv2.waitKey()
-
-# Lazer to Camera 28.541
-#Axis to camera 21.072degrees, 40.212, distnace 14.458, long 37.523
